113e9549134f021cc63baaa3c8c7df633c611530
tom <tom>
2018-06-14 07:41:17 -0700

    added temple files added test data added basic style

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Thomas Duda',
+		'title': 'Blog Poast 1',
+		'content': 'First post content',
+		'date_posted': 'June 14, 2018'
+	},
+	{
+		'author': 'Sabastian Duda',
+		'title': 'Blog Poast 2',
+		'content': 'Second post content',
+		'date_posted': 'June 15, 2018'
+	}
+
+]
 
 # Routing
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Sebastian's Homepage</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>Sebastian's About Page</h1>"
+    return render_template('about.html', title="About")
 
 # Runs in debug mode only if the script is run directly
 # This is for development purposes so I don't have to constantly restart the flask server
