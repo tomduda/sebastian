35c54eb98569e5b07b00404fb4d9f76f3bb809aa
tom <tom>
2018-06-17 05:33:29 -0700

    Added an image uploader for user profiles

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -30,3 +32,23 @@ class LoginForm(FlaskForm):
 	password = PasswordField('password', validators=[DataRequired() ])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Username', 
+							validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(),Email()])
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg',''])])
+	submit = SubmitField('Update')
+
+	def validate_username(self,username):
+		if username.data != current_user.username:
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError('That username is taken. Plase choose a new one')
+
+	def validate_email(self,email):
+		if email.data != current_user.email:
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError('That email is taken. Plase choose a new one')
